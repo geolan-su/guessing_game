0d07f3ff6a823d788fdddf7d10e76dc47e16c11c
geolan-su <georgelantin@gmail.com>
2022-04-28 09:59:53 -0500

    rust practice

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is {}", secret_number);
+    println!("Input your giss, thanks.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    println!("YOu guessed: {}", guess);
+    
+}
