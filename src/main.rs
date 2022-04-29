93df2b20283d26b1dcad80a084262019547fe221
geolan-su <georgelantin@gmail.com>
2022-04-29 09:38:04 -0500

    final program - it just works

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    
-    println!("The secret number is {}", secret_number);
+
 
     loop {
         println!("Input your giss, thanks.");
@@ -19,8 +18,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type number bro");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("YOu guessed: {}", guess);
 
