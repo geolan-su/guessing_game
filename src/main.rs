63a4702ed473202ea3fc2ce57cadf58f2f4144c4
geolan-su <georgelantin@gmail.com>
2022-04-29 09:26:59 -0500

    made it so the loop dies and stuff compiles

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,33 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is {}", secret_number);
-    println!("Input your giss, thanks.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
     
-    println!("YOu guessed: {}", guess);
+    println!("The secret number is {}", secret_number);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Input your giss, thanks.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type number bro");
+
+        println!("YOu guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
     }
-
 }
 
 
